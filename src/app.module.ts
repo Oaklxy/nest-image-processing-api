28b39f0a0ee2e58aa645849964126f78b1aced13
Jefferson <nekz.8896@gmail.com>
2025-07-21 12:31:21 -0500

    feat: add prisma module and prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { envValidationsSchema } from './config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { envValidationsSchema } from './config';
       expandVariables: true,
       validationSchema: envValidationsSchema,
     }),
+    PrismaModule,
   ],
 })
 export class AppModule { };
