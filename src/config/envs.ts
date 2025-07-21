496804104f6e2bd18b5e05751b91adb19a6ab0d9
Jefferson <nekz.8896@gmail.com>
2025-07-21 12:33:19 -0500

    feat: add environment variable validations with joi

---
+++ src/config/envs.ts
@@ -0,0 +1,26 @@
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: string;
+
+  DATABASE_HOST: string;
+  DATABASE_NAME: string;
+  DATABASE_PORT: string;
+  DATABASE_PASSWORD: string;
+  DATABASE_USER: string;
+
+  JWT_SECRET: string;
+};
+
+export const envValidationsSchema = joi.object<EnvVars>({
+  PORT: joi.string().required(),
+
+  DATABASE_HOST: joi.string().required(),
+  DATABASE_NAME: joi.string().required(),
+  DATABASE_PORT: joi.string().required(),
+  DATABASE_PASSWORD: joi.string().required(),
+  DATABASE_USER: joi.string().required(),
+
+  JWT_SECRET: joi.string().required(),
+})
+  .unknown(true);
