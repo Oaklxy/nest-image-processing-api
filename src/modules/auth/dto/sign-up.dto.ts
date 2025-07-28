ef91bcba543447a477c70b61f612ba86f2be8b99
Jefferson <nekz.8896@gmail.com>
2025-07-28 03:29:41 -0500

    refactor: remove ApiProperty decorator from dto attributes

--- src/modules/auth/dto/sign-up.dto.ts
+++ src/modules/auth/dto/sign-up.dto.ts
@@ -1,16 +1,12 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
 
 export class SignUpDto {
-  @ApiProperty()
   @IsString()
   public name: string;
 
-  @ApiProperty()
   @IsEmail()
   public email: string;
 
-  @ApiProperty()
   @IsString()
   @IsStrongPassword()
   public password: string;
