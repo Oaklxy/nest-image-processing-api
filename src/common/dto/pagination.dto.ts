1378b758c42f6c7e97a55f1adfe368f998f22664
Jefferson <nekz.8896@gmail.com>
2025-07-30 16:42:31 -0500

    docs: add swagger documentation

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    description: 'Pagination page',
+    default: 1,
+    example: 1,
+    required: false,
+  })
   @IsOptional()
   @IsPositive()
   @Type(() => Number)
   public page: number = 1;
 
+  @ApiProperty({
+    description: 'Pagination limit',
+    default: 10,
+    example: 10,
+    required: false,
+  })
   @IsOptional()
   @IsPositive()
   @Type(() => Number)
