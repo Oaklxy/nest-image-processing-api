d389a9eb1a05e12eff82c96b931d8cc51e4c4b51
Jefferson <nekz.8896@gmail.com>
2025-07-30 16:28:39 -0500

    docs: add swagger documentation

--- src/modules/auth/dto/sign-up.dto.ts
+++ src/modules/auth/dto/sign-up.dto.ts
@@ -1,13 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, IsStrongPassword, MinLength } from 'class-validator';
 
 export class SignUpDto {
+  @ApiProperty({
+    description: 'User name',
+    example: 'Jefferson',
+    required: true,
+  })
   @IsString()
   @MinLength(3)
   public name: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'oaklxy@oaklxy.com',
+    required: true,
+    uniqueItems: true,
+  })
   @IsEmail()
   public email: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'mypassword123',
+    required: true,
+  })
   @IsString()
   @IsStrongPassword()
   public password: string;
