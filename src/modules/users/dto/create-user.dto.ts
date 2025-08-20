93247e4ac069eb318f74318442ae2b0030d0ae37
Jefferson <nekz.8896@gmail.com>
2025-08-19 22:49:53 -0500

    docs: add documentation for dto and entities

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,12 +1,32 @@
-import { IsEmail, IsString, IsStrongPassword } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User name',
+    example: 'Oakley',
+    required: true,
+    nullable: false,
+  })
   @IsString()
   public name: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'oakley@oakley.com',
+    required: true,
+    nullable: false,
+    uniqueItems: true,
+  })
   @IsEmail()
   public email: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'EncryptedHash',
+    required: true,
+    nullable: false,
+  })
   @IsStrongPassword()
   public password: string;
 };
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -12,6 +12,7 @@ export class UserEntity implements User {
   @ApiProperty({
     description: 'User name',
     example: 'Jefferson',
+    nullable: false,
     required: true,
   })
   public name: string;
@@ -19,14 +20,16 @@ export class UserEntity implements User {
   @ApiProperty({
     description: 'User email',
     example: 'oaklxy@oaklxy.com',
-    uniqueItems: true,
+    nullable: false,
     required: true,
+    uniqueItems: true,
   })
   public email: string;
 
   @ApiProperty({
     description: 'User password',
     example: 'Hash: $argon2id$v=19$m=65536,t=3,p=4$sRs66SYNvkaHf4fLouuJag$TVBoVDAqaFcjrHKcAT5fekYddCKngF/u1RqwkO7CuoE',
+    nullable: false,
     required: true,
   })
   public password: string;
@@ -34,16 +37,16 @@ export class UserEntity implements User {
   @ApiProperty({
     description: 'User JWT refresh token',
     example: 'Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc0NTExYTZhLWY2NmMtNGE1NS1hZjdiLWJkYTk1ZTY0YWNjMSIsImVtYWlsIjoib2FrbHh5QG9ha2x4eS5jb20iLCJpYXQiOjE3NTM3NjU3ODIsImV4cCI6MTc1Mzg1MjE4Mn0.p1dbqdNbQOo-klOpIIuCE2HkRK7BunWpjUKC2NttBTE',
-    uniqueItems: true,
-    nullable: true, 
+    nullable: true,
     required: false, 
+    uniqueItems: true,
   })
   public refresh_token: string | null;
 
   @ApiProperty({
     description: 'User is active status',
-    example: 'true',
     default: true,
+    example: 'true',
     required: false,
   })
   public is_active: boolean;
@@ -58,6 +61,7 @@ export class UserEntity implements User {
   @ApiProperty({
     description: 'User updated at date',
     example: '2025-07-29T05:09:42.340Z',
+    nullable: false,
     required: false,
   })
   public updated_at: Date;
