78c1e8ec4c0c01d59be94192608ab70bd667c30e
Jefferson <nekz.8896@gmail.com>
2025-08-24 22:17:21 -0500

    feat: add transform images dto

--- src/modules/images/dto/transform-images.dto.ts
+++ src/modules/images/dto/transform-images.dto.ts
@@ -1,7 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, ValidateNested } from 'class-validator';
 
-export class TransformImagesDto {
+class ResizeDto {
+  @ApiProperty({
+    description: 'The desired height of the image',
+    type: Number,
+    required: false,
+  })
+  @IsNumber()
+  @IsOptional()
+  public height?: number;
+
   @ApiProperty({
     description: 'The desired width of the image',
     type: Number,
@@ -10,11 +20,42 @@ export class TransformImagesDto {
   @IsNumber()
   @IsOptional()
   public width?: number;
+};
 
+class CropDto {
   @ApiProperty({
-    description: 'The desired height of the image',
+    description: 'The new desired height for the image',
     type: Number,
     required: false,
   })
+  @IsNumber()
+  @IsOptional()
   public height?: number;
+
+  @ApiProperty({
+    description: 'The new desired width for the image',
+    type: Number,
+    required: false,
+  })
+  @IsNumber()
+  @IsOptional()
+  public width?: number;
+};
+
+class TransformationsDto {
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => CropDto)
+  public crop?: CropDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => ResizeDto)
+  public resize?: ResizeDto;
+};
+
+export class TransformImagesDto {
+  @ValidateNested()
+  @Type(() => TransformationsDto)
+  public transformations?: TransformationsDto;
 };
